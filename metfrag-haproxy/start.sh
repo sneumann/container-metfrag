60997177b007c7029feb0bafad6e4f06a8d85af2
korseby <kristian@funkel.ipb-sub.ipb-halle.de>
2016-05-27 08:55:07 +0200

    added docker-compose haproxy

---
+++ metfrag-haproxy/start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HAPROXY="/etc/haproxy"
+OVERRIDE="/haproxy-override"
+PIDFILE="/var/run/haproxy.pid"
+
+CONFIG="haproxy.cfg"
+ERRORS="errors"
+
+cd "$HAPROXY"
+
+# Symlink errors directory
+if [[ -d "$OVERRIDE/$ERRORS" ]]; then
+  mkdir -p "$OVERRIDE/$ERRORS"
+  rm -fr "$ERRORS"
+  ln -s "$OVERRIDE/$ERRORS" "$ERRORS"
+fi
+
+# Symlink config file.
+if [[ -f "$OVERRIDE/$CONFIG" ]]; then
+  rm -f "$CONFIG"
+  ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
+fi
+
+# Create hosts
+for ((i=2;i<=99;i++)); do
+  echo "10.38.38.$i metfrag$i dockermetfrag_metfrag_$i" >> /etc/hosts
+done
+
+exec haproxy -f /etc/haproxy/haproxy.cfg -p "$PIDFILE"
+
